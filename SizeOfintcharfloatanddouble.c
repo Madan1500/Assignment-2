41f32b9845be9e8092392e0a87ad374532482d4d
Madan1500 <132214432+Madan1500@users.noreply.github.com>
2023-06-18 11:57:48 +0530

    Add files via upload

---
+++ SizeOfintcharfloatanddouble.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Size of int type variable is %d.\n",sizeof(int));
+    printf("Size of char type variable is %d.\n",sizeof(char));
+    printf("Size of float type variable is %d.\n",sizeof(float));
+    printf("Size of double type variable is %d.\n",sizeof(double));
+    return 0;
+}
